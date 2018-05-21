6708b2bb3540b6f5c2d2ea4601e3b310534a7f8c
Eddie Montooth <edmon@udel.edu>
2018-05-21 17:38:55 -0400

    Second go at this

--- moviebase.py
+++ moviebase.py
@@ -3,14 +3,21 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def index():
+def homepage():
     #return "<h1> This is not the first page</h1>"
-    return render_template('index.html')
+    return render_template('homepage.html')
 
 
 @app.route('/users/<string:name>/')
 def get_user_name(name):
     return render_template('users.html')
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
 
 
 if __name__ == '__main__':
